643f119616282345e4d7896505557c021bbbac20
lhgravendeel <leenderthenk@gmail.com>
2020-07-11 22:07:59 +0200

    Adding support for Shelly MQTT to Domoticz / generic URL publishing

--- src/main/java/nl/gravendeeldesign/robodomus/Application.java
+++ src/main/java/nl/gravendeeldesign/robodomus/Application.java
@@ -11,11 +11,14 @@ import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
 
 import java.io.File;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.concurrent.ArrayBlockingQueue;
 import java.util.concurrent.BlockingQueue;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 
 /**
  * Simple application to receive zigbee2mqtt button presses and convert it into lamp actions.
@@ -71,12 +74,24 @@ public class Application {
     ConfigObject groups = config.getObject("groups");
     for(Map.Entry<String, ConfigValue> group : groups.entrySet()) {
       String groupName = group.getKey();
+      System.out.println("Connecting to MQTT for lighting group: " + groupName);
       Config groupConf = config.getConfig("groups." + groupName);
       List<String> buttons = groupConf.getStringList("buttons");
       List<String> lamps = groupConf.getStringList("lamps");
       for(String button : buttons) {
         startConsumer(button, lamps);
       }
+    }
+
+    ConfigObject shellies = config.getObject("shellies");
+    for(Map.Entry<String, ConfigValue> shelly : shellies.entrySet()) {
+      String shellyName = shelly.getKey();
+      Config shellyConf = config.getConfig("shellies." + shellyName);
+      String power = shellyConf.getString("power");
+      String energy = shellyConf.getString("energy");
+      System.out.println("Connecting to MQTT for Shelly: " + shellyName + ", " + power + ", " + energy);
+      String url = shellyConf.getString("url");
+      startShellyConsumer(power, energy, url);
     }
 
     while(isRunning) {
@@ -119,6 +134,91 @@ public class Application {
         ex.printStackTrace();
       }
     });
+  }
+
+  /**
+   * Start a shelly consumer
+   * @param powerTopic Power topic
+   * @param energyTopic
+   * @param urlPattern
+   * @throws MqttException
+   */
+  void startShellyConsumer(String powerTopic, String energyTopic, String urlPattern) throws MqttException {
+    AtomicLong powermW = new AtomicLong(-1);// Power in milliwatt
+    AtomicLong energyWm = new AtomicLong(-1);// Energy in Watt-minutes
+    AtomicLong lastPower = new AtomicLong(0);
+    AtomicLong lastEnergy = new AtomicLong(0);
+
+    client.subscribe(powerTopic, (topic, message) -> {
+      String msg = new String(message.getPayload(), StandardCharsets.UTF_8);
+      if(msg != null && !msg.trim().isEmpty()) {
+        powermW.set(Math.round(Double.valueOf(msg) * 1000.0));
+      }
+      long age = System.currentTimeMillis() - lastPower.get();
+      if(powermW.get() >= 0 && energyWm.get() >= 0 && age >= 5_000) {// Don't send more often than once per 5 seconds
+        lastPower.set(System.currentTimeMillis());
+        sendShellyStatsAsync(urlPattern, powermW.get() / 1000.0, energyWm.get() / 60.0);
+      }
+    });
+    client.subscribe(energyTopic, (topic, message) -> {
+      String msg = new String(message.getPayload(), StandardCharsets.UTF_8);
+      if(msg != null && !msg.trim().isEmpty()) {
+        energyWm.set(Math.round(Double.valueOf(msg)));
+      }
+      long age = System.currentTimeMillis() - lastEnergy.get();
+      if(powermW.get() >= 0 && energyWm.get() >= 0 && age >= 5_000) {// Don't send more often than once per 5 seconds
+        lastEnergy.set(System.currentTimeMillis());
+        sendShellyStatsAsync(urlPattern, powermW.get() / 1000.0, energyWm.get() / 60.0);
+      }
+    });
+  }
+
+  /**
+   * Send Shelly stats asynchronously.
+   * @param urlPattern
+   * @param powerW
+   * @param energyWh
+   */
+  void sendShellyStatsAsync(String urlPattern, double powerW, double energyWh) {
+    try {
+      String url = urlPattern
+          .replace("%POWER_WATT%", String.format("%.0f", powerW))
+          .replace("%ENERGY_WATTHOUR%", String.format("%.0f", energyWh));
+
+      String authHeader = null;
+      if(url.startsWith("http") && url.contains("@")) {
+        String authStr = url.substring(url.indexOf("//") + 2);
+        authStr = authStr.substring(0, authStr.indexOf("@"));
+        byte[] encodedAuth = Base64.getEncoder().encode(authStr.getBytes(StandardCharsets.UTF_8));
+        authHeader = "Basic " + new String(encodedAuth, StandardCharsets.UTF_8);
+
+        url = url.substring(0, url.indexOf("//")) + "//" + url.substring(url.indexOf("@") + 1);
+      }
+      final String urlStr = url;
+      final String authHeaderValue = authHeader;
+
+      queue.add(() -> {
+        try {
+          URL urlObj = new URL(urlStr);
+
+          HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
+          conn.setRequestMethod("GET");
+          conn.setConnectTimeout(5000);
+          conn.setReadTimeout(5000);
+          conn.setInstanceFollowRedirects(false);
+          if(authHeaderValue != null) {
+            conn.setRequestProperty("Authorization", authHeaderValue);
+          }
+          int responseCode = conn.getResponseCode();
+          conn.disconnect();
+
+        } catch (Exception ex) {
+          ex.printStackTrace();//TODO: Handle
+        }
+      });
+    } catch(Exception ex) {
+      ex.printStackTrace();
+    }
   }
 
   /**
